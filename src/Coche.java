cee87fada53d44060e6f1badbfc771e86b8900eb
bracket2code <bracket2code@gmail.com>
2022-12-07 03:29:51 +0100

    Initial Release

---
+++ src/Coche.java
@@ -0,0 +1,7 @@
+public class Coche {
+    int nPuertas;
+
+    public void anadirPuerta() {
+        nPuertas++;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultadosuma = suma(10,20,30);
+        System.out.println(resultadosuma);
+        Coche miCoche = new Coche();
+        miCoche.anadirPuerta();
+        System.out.println(miCoche.nPuertas);
+
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
